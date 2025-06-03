b9599dc2826c48b31bd0377d2b0f8f28d87213be
YasunoriKamata <yasunorikamata0715@yahoo.co.jp>
2025-06-03 23:39:24 +0900

    出勤簿データ確認ページに行追加ボタンを追加し、記入者と監視長の入力フィールドをテキストボックスからスパンに変更。スタイルを調整し、テーブルの幅を拡大。データ表示処理を改善。

--- check-status/script.js
+++ check-status/script.js
@@ -19,7 +19,8 @@ $(document).ready(function () {
   // 全データを取得
   fetchAllData();
 
-  // 行の動的追加
+  // ヘッダの初期化・行の動的追加
+  initHeaders();
   initRow();
 
   // 今日の日付を設定
@@ -29,7 +30,15 @@ $(document).ready(function () {
   initDragScroll();
 });
 
-// テーブルの行を初期化する関数
+// 全ての記入者・監視長の初期値を初期化
+function initHeaders() {
+  for (let i = 1; i <= 4; i++) {
+    document.getElementById(`recorder${i}`).textContent = '-';
+    document.getElementById(`supervisor${i}`).textContent = '-';
+  }
+}
+
+// テーブルの行を初期化
 function initRow() {
   // 一度にすべてのテーブルボディをクリア
   const tableBodies = [
@@ -94,6 +103,13 @@ function setLoading(isLoading) {
 async function fetchAllData() {
   try {
     setLoading(true);
+    Swal.fire({
+      title: 'データ取得中...',
+      allowOutsideClick: false,
+      didOpen: () => {
+        Swal.showLoading();
+      }
+    });
 
     console.log(`全データを取得開始：[${getJSTISOString()}]`);
     const responseData = await fetchData();
@@ -114,6 +130,7 @@ async function fetchAllData() {
   } catch (error) {
     console.error(`データ取得処理失敗：[${getJSTISOString()}]：`, error);
   } finally {
+    Swal.close();
     setLoading(false);
   }
 }
@@ -148,6 +165,7 @@ async function checkStatus() {
       return;
     }
 
+    initHeaders(); // ヘッダを初期化
     initRow(); // まずは初期化
 
     // 全データから選択された日付のデータを取得
@@ -164,24 +182,24 @@ async function checkStatus() {
 
 // データを画面に表示
 function updateDisplay(data) {
-  updateLocationInfo('1', data.morito);
-  updateLocationInfo('2', data.isshiki);
-  updateLocationInfo('3', data.chojagasaki);
-  updateLocationInfo('4', data.event);
+  displayLocationInfo('1', data.morito);
+  displayLocationInfo('2', data.isshiki);
+  displayLocationInfo('3', data.chojagasaki);
+  displayLocationInfo('4', data.event);
 }
 
 // 各監視所の情報を更新する関数
-function updateLocationInfo(index, locationData) {
+function displayLocationInfo(index, locationData) {
   if (!locationData) {
     return; // locationDataがnullまたはundefinedの場合は処理をスキップ
   }
 
-  const writerElement = document.getElementById(`writer${index}`);
+  const recorderElement = document.getElementById(`recorder${index}`);
   const supervisorElement = document.getElementById(`supervisor${index}`);
   const tableBody = document.getElementById(`attendanceTableBody${index}`);
 
-  if (writerElement && supervisorElement) {
-    writerElement.textContent = locationData.writer;
+  if (recorderElement && supervisorElement) {
+    recorderElement.textContent = locationData.writer;
     supervisorElement.textContent = locationData.supervisor;
   }
 
@@ -197,14 +215,19 @@ function updateLocationInfo(index, locationData) {
       locationData.details.forEach(detail => {
         const row = document.createElement('tr');
         row.innerHTML = `
-          <td>${detail.name || ''}</td>
-          <td>${detail.volunteer == 1 ? '✓' : ''}</td>
-          <td>${detail.type == 1 ? '日勤' : '時間勤'}</td>
-          <td>${detail.startTime || ''}</td>
-          <td>${detail.endTime || ''}</td>
-          <td>${detail.batchTest == 1 ? '✓' : ''}</td>
-          <td>${detail.remarks || ''}</td>
-        `;
+        <td><input type="text" value="${detail.name || ''}" /></td>
+        <td><input type="checkbox" ${detail.volunteer == 1 ? 'checked' : ''} /></td>
+        <td>
+          <select>
+            <option value="1" ${detail.type == 1 ? 'selected' : ''}>日勤</option>
+            <option value="2" ${detail.type == 2 ? 'selected' : ''}>時間勤</option>
+          </select>
+        </td>
+        <td><input type="time" list="data-list" value="${detail.startTime || ''}" /></td>
+        <td><input type="time" list="data-list" value="${detail.endTime || ''}" /></td>
+        <td><input type="checkbox" ${detail.batchTest == 1 ? 'checked' : ''} /></td>
+        <td><input type="text" value="${detail.remarks || ''}" /></td>
+      `;
         fragment.appendChild(row);
       });
 
@@ -213,6 +236,138 @@ function updateLocationInfo(index, locationData) {
       // データがない場合は空の行を表示
       tableBody.innerHTML = ROW_TEMPLATE;
     }
+  }
+}
+
+// ボタン処理
+function addRow(location) {
+  //１行しかないときは、削除してから
+  const tableBody = document.getElementById(`attendanceTableBody${location}`);
+  if (tableBody && tableBody.children.length === 1) {
+    tableBody.innerHTML = '';
+  }
+  // 新しい行を追加
+  if (tableBody) {
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `
+      <td><input type="text" value="" /></td>
+      <td><input type="checkbox" /></td>
+      <td>
+        <select>
+          <option value="1">日勤</option>
+          <option value="2">時間勤</option>
+        </select>
+      </td>
+      <td><input type="time" list="data-list" value="" /></td>
+      <td><input type="time" list="data-list" value="" /></td>
+      <td><input type="checkbox" /></td>
+      <td><input type="text" value="" /></td>
+    `;
+    tableBody.appendChild(newRow);
+  }
+}
+
+async function execUpdate(location) {
+  let msg = '';
+  const selectedDate = document.getElementById('date').value.replace(/-/g, '/');
+  if (!selectedDate) {
+    alert('日付を選択してください');
+    return;
+  }
+  switch (location) {
+    case 1:
+      msg = '森戸海岸'
+      break;
+    case 2:
+      msg = '一色海岸'
+      break;
+    case 3:
+      msg = '長者ヶ崎海岸'
+      break;
+    case 4:
+      msg = 'イベント'
+      break;
+  }
+
+  alert(`${msg}：${selectedDate} の出勤簿データを修正登録します。`);
+
+  try {
+    setLoading(true);
+    console.log(`データ削除処理開始：[${getJSTISOString()}]`);
+    //削除データ作成
+    const deleteData = {
+      type: 'delete',
+      date: selectedDate,
+      location: `${location}`
+    };
+    //削除処理実行
+    let response = await fetch(GAS_URL, {
+      method: 'POST',
+      mode: 'cors',
+      body: JSON.stringify(deleteData)
+    });
+
+    let result = await response.json();
+    if (result.status === 'error') {
+      //削除データがない場合などはここに入る
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: result.message,
+      });
+      console.log(`出勤簿データ登録失敗 削除データ無し？：[${getJSTISOString()}]`);
+      return
+    }
+    console.log(`データ削除処理完了：[${getJSTISOString()}]`);
+    console.log(`データ登録処理開始：[${getJSTISOString()}]`);
+
+    // 登録データ作成
+    const tableBody = document.getElementById(`attendanceTableBody${location}`);
+    const rows = tableBody.querySelectorAll('tr');
+    const details = Array.from(rows).map(row => {
+      const inputs = row.querySelectorAll('input, select');
+      return {
+        name: inputs[0].value.trim(),
+        volunteer: inputs[1].checked ? 1 : 0,
+        shiftType: parseInt(inputs[2].value, 10),
+        startTime: inputs[3].value.trim(),
+        endTime: inputs[4].value.trim(),
+        batchTest: inputs[5].checked ? 1 : 0,
+        remarks: inputs[6].value.trim()
+      };
+    }).filter(detail => detail.name !== '');
+    const requestData = {
+      type: 'regist',
+      date: selectedDate,
+      location: `${location}`,
+      recorder: document.getElementById(`recorder${location}`).textContent.trim(),
+      supervisor: document.getElementById(`supervisor${location}`).textContent.trim(),
+      details: details
+    };
+
+    response = await fetch(GAS_URL, {
+      method: 'POST',
+      mode: 'cors',
+      body: JSON.stringify(requestData)
+    });
+    result = await response.json();
+
+    switch (result.status) {
+      case 'success':
+        // 登録成功
+        console.log(`データ登録処理完了：[${getJSTISOString()}]`);
+        alert(`データ登録処理完了：[${getJSTISOString()}]`);
+        break;
+      case 'error':
+        // 既存データがある場合などはここに入る
+        console.log(`データ登録処理失敗 既存データ有り？：[${getJSTISOString()}]`);
+        alert(`データ登録処理失敗：[${getJSTISOString()}]`);
+        return;
+    }
+  } catch (error) {
+    console.error(`データ更新処理失敗：[${getJSTISOString()}]：`, error);
+  } finally {
+    setLoading(false);
   }
 }
 
