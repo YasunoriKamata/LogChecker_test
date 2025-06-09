ad50840bc25f3679e2d4330a2f4cdade5536f9d2
YasunoriKamata <yasunorikamata0715@yahoo.co.jp>
2025-06-09 20:42:06 +0900

    出勤簿データ確認ページの行追加ボタンを削除し、記入者と監視長の入力フィールドをスパンに変更。スタイルを調整し、テーブルの幅を微調整。データ取得処理を改善。

--- check-status/script.js
+++ check-status/script.js
@@ -33,18 +33,8 @@ $(document).ready(function () {
 // 全ての記入者・監視長の初期値を初期化
 function initHeaders() {
   for (let i = 1; i <= 4; i++) {
-<<<<<<< HEAD
-    document.getElementById(`writer${i}`).value = '-';
-    document.getElementById(`supervisor${i}`).value = '-';
-=======
-<<<<<<< HEAD
     document.getElementById(`recorder${i}`).textContent = '-';
     document.getElementById(`supervisor${i}`).textContent = '-';
-=======
-    document.getElementById(`writer${i}`).value = '-';
-    document.getElementById(`supervisor${i}`).value = '-';
->>>>>>> cec5aea6b150e679a3414f0ee35030ebfdf18568
->>>>>>> 3f0242f016b446c51c4fa0dda93cc27b536012f9
   }
 }
 
@@ -208,21 +198,9 @@ function displayLocationInfo(index, locationData) {
   const supervisorElement = document.getElementById(`supervisor${index}`);
   const tableBody = document.getElementById(`attendanceTableBody${index}`);
 
-<<<<<<< HEAD
-  if (writerElement && supervisorElement) {
-    writerElement.value = locationData.writer;
-    supervisorElement.value = locationData.supervisor;
-=======
-<<<<<<< HEAD
   if (recorderElement && supervisorElement) {
     recorderElement.textContent = locationData.writer;
     supervisorElement.textContent = locationData.supervisor;
-=======
-  if (writerElement && supervisorElement) {
-    writerElement.value = locationData.writer;
-    supervisorElement.value = locationData.supervisor;
->>>>>>> cec5aea6b150e679a3414f0ee35030ebfdf18568
->>>>>>> 3f0242f016b446c51c4fa0dda93cc27b536012f9
   }
 
   // テーブルの内容をクリア
@@ -262,9 +240,6 @@ function displayLocationInfo(index, locationData) {
 }
 
 // ボタン処理
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 function addRow(location) {
   //１行しかないときは、削除してから
   const tableBody = document.getElementById(`attendanceTableBody${location}`);
@@ -292,9 +267,6 @@ function addRow(location) {
   }
 }
 
-=======
->>>>>>> cec5aea6b150e679a3414f0ee35030ebfdf18568
->>>>>>> 3f0242f016b446c51c4fa0dda93cc27b536012f9
 async function execUpdate(location) {
   let msg = '';
   const selectedDate = document.getElementById('date').value.replace(/-/g, '/');
@@ -329,29 +301,13 @@ async function execUpdate(location) {
       location: `${location}`
     };
     //削除処理実行
-<<<<<<< HEAD
-    const response = await fetch(GAS_URL, {
-=======
-<<<<<<< HEAD
     let response = await fetch(GAS_URL, {
-=======
-    const response = await fetch(GAS_URL, {
->>>>>>> cec5aea6b150e679a3414f0ee35030ebfdf18568
->>>>>>> 3f0242f016b446c51c4fa0dda93cc27b536012f9
       method: 'POST',
       mode: 'cors',
       body: JSON.stringify(deleteData)
     });
 
-<<<<<<< HEAD
-    const result = await response.json();
-=======
-<<<<<<< HEAD
     let result = await response.json();
-=======
-    const result = await response.json();
->>>>>>> cec5aea6b150e679a3414f0ee35030ebfdf18568
->>>>>>> 3f0242f016b446c51c4fa0dda93cc27b536012f9
     if (result.status === 'error') {
       //削除データがない場合などはここに入る
       Swal.fire({
@@ -365,12 +321,6 @@ async function execUpdate(location) {
     console.log(`データ削除処理完了：[${getJSTISOString()}]`);
     console.log(`データ登録処理開始：[${getJSTISOString()}]`);
 
-<<<<<<< HEAD
-
-
-    console.log(`データ登録処理完了：[${getJSTISOString()}]`);
-=======
-<<<<<<< HEAD
     // 登録データ作成
     const tableBody = document.getElementById(`attendanceTableBody${location}`);
     const rows = tableBody.querySelectorAll('tr');
@@ -414,12 +364,6 @@ async function execUpdate(location) {
         alert(`データ登録処理失敗：[${getJSTISOString()}]`);
         return;
     }
-=======
-
-
-    console.log(`データ登録処理完了：[${getJSTISOString()}]`);
->>>>>>> cec5aea6b150e679a3414f0ee35030ebfdf18568
->>>>>>> 3f0242f016b446c51c4fa0dda93cc27b536012f9
   } catch (error) {
     console.error(`データ更新処理失敗：[${getJSTISOString()}]：`, error);
   } finally {
